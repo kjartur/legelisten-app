297472167240e3f808c9c8ea443821fbf2fe0410
Kjartan <kjartan.manvelyan@gmail.com>
2022-03-21 10:04:07 +0000

    Create component to show clinics

--- app/javascript/components/Clinics.jsx
+++ app/javascript/components/Clinics.jsx
@@ -22,6 +22,61 @@ class Clinics extends React.Component {
       .then(response => this.setState({ clinics: response }))
       .catch(() => this.props.history.push("/"));
 }
+render() {
+    const { clinics } = this.state;
+    const allClinics = clinics.map((clinic, index) => (
+      <div key={index} className="col-md-6 col-lg-4">
+        <div className="card mb-4">
+          <img
+            src={clinic.image}
+            className="card-img-top"
+            alt={`${clinic.name} image`}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{clinic.name}</h5>
+            <Link to={`/clinic/${clinic.id}`} className="btn custom-button">
+              View Clinic
+            </Link>
+          </div>
+        </div>
+      </div>
+    ));
+    const noClinic = (
+      <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
+        <h4>
+          No registered clinics. <Link to="/new_clinic">Add clinic</Link>
+        </h4>
+      </div>
+    );
+
+    return (
+      <>
+        <section className="jumbotron jumbotron-fluid text-center">
+          <div className="container py-5">
+            <h1 className="display-4">Legelisten</h1>
+            <p className="lead text-muted">
+              We work with the best medical professionals in the field.
+            </p>
+          </div>
+        </section>
+        <div className="py-5">
+          <main className="container">
+            <div className="text-right mb-3">
+              <Link to="/clinic" className="btn custom-button">
+                Create New Clinic
+              </Link>
+            </div>
+            <div className="row">
+              {clinics.length > 0 ? allClinics : noClinic}
+            </div>
+            <Link to="/" className="btn btn-link">
+              Home
+            </Link>
+          </main>
+        </div>
+      </>
+    );
+  }
 
 }
 export default Clinics;
