429547fdbfb2869489213f340119817b004f9dbe
Kjartan <kjartan.manvelyan@gmail.com>
2022-03-21 10:06:10 +0000

    Add routes for clinics

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Routes, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
+import Clinics from "../components/Clinics";
 
 export default (
   <Routes>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/clinics" exact component={Clinics} />
     </Switch>
   </Routes>
 );
