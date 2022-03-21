56e7e516ea18ca3373aebbad5b261bbe1ac907d7
Kjartan <kjartan.manvelyan@gmail.com>
2022-03-21 08:53:11 +0000

    Add read and create + strong params for controller

--- app/controllers/api/v1/clinics_controller.rb
+++ app/controllers/api/v1/clinics_controller.rb
@@ -1,13 +1,27 @@
 class Api::V1::ClinicsController < ApplicationController
   def index
+    clinic = Clinic.all.order(created_at: :desc)
+    render json: clinic
   end
 
   def create
+    clinic = Clinic.create!(clinic_params)
+    if recipe
+      render json: clinic
+    else
+      render json: clinic.errors
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  def clinic_params
+    params.permit(:name, :image, :about, :address)
   end
 end
