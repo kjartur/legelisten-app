5da58fe48d6023d6a3ba46fe26d98be4419553a5
Kjartan <kjartan.manvelyan@gmail.com>
2022-03-21 08:38:01 +0000

    Modify migration file, add default image

--- db/migrate/20220321081755_create_clinics.rb
+++ db/migrate/20220321081755_create_clinics.rb
@@ -1,10 +1,10 @@
 class CreateClinics < ActiveRecord::Migration[6.1]
   def change
     create_table :clinics do |t|
-      t.string :name
-      t.text :address
-      t.text :about
-      t.string :image
+      t.string :name, null: false
+      t.text :address, null: false
+      t.text :about, null: false
+      t.string :image, default: 'https://nekolighting.com/wp-content/uploads/2020/02/Kardiologen-Celle-2XII2734-web.jpg'
 
       t.timestamps
     end
