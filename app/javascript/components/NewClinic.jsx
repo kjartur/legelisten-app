604f78748726a7e18c9795990fd2e5b72382ba55
Kjartan <kjartan.manvelyan@gmail.com>
2022-03-21 10:42:13 +0000

    Added onChange and onSubmit methods + redirect to newly created clinic

--- app/javascript/components/NewClinic.jsx
+++ app/javascript/components/NewClinic.jsx
@@ -20,7 +20,43 @@ class NewClinic extends React.Component {
       .replace(/</g, "&lt;")
       .replace(/>/g, "&gt;");
   }
-  
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const url = "/api/v1/clinics/create";
+    const { name, address, about } = this.state;
+
+    if (name.length == 0 || address.length == 0 || about.length == 0)
+      return;
+
+    const body = {
+      name,
+      address,
+      about
+    };
+
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => this.props.history.push(`/clinic/${response.id}`))
+      .catch(error => console.log(error.message));
+  }
 }
 
 export default NewClinic;
